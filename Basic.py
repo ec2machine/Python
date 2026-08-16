e0fc72d42cce0d5db31d954cd0e1acf11398956f
ec2machine <ec2.machine5454@gmail.com>
2026-08-16 21:56:46 +0530

    16 aug 26

--- Basic.py
+++ Basic.py
@@ -1,1 +1,38 @@
+# print Hello world::
+
 print("hello world")
+
+#Variables::
+
+name= "vigneshwaran"
+_name='vigneshwaran'
+name_age='vignesh,22'
+NAME="vigneshwaran"
+print(NAME)
+#1name='sting' -- dont use declaer this method
+
+#Data Types ::
+
+init=4
+
+floar=4.0
+
+str="vigneshwaran"
+
+bool=True #False
+
+list=["vignesh",12, True] # Ordered(index), mutable, collection, allow dublicate elements
+
+tuple=("vigneshwaran",12, False) # orderes(index), immutable, collection, allow dublicate elements
+
+set={"vigneshwaran",12, True, 12.0} # unordered, mutable, collection, not allow dublicate elements
+
+dict={"name":"vigneshwaran","age":22,"ismale":True} # ordereed, mutable, not allow key dublicate, collection
+
+None #No value
+
+print(list,tuple,set,dict)
+
+
+ 
+
