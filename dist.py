2bb7478acfd114d3c4f2339c9bbfef3d55e4d033
ec2machine <ec2.machine5454@gmail.com>
2026-08-17 10:56:40 +0530

    dict

---
+++ dist.py
@@ -0,0 +1,8 @@
+dict={"name":"vigneshwaran","age":22,"addres":{
+    "doorno":124,"city":"komarapalayam"}}
+
+print(dict.keys())
+print(dict.values())
+print(dict.items())
+
+print(dict[addres["city"]["doorno"]])
